429e131768dca4f0d3697cd0748a2b295be62d1a
Ravik Ganguly <theravikganguly@gmail.com>
2022-01-28 14:01:14 +0530

    fix: base route error

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,10 +1,8 @@
 import { IRequest, IResponse } from '../types/General';
 import { decoratedRouter as router } from '../utils/router';
 
-const BASE_ROUTE = '/';
-
 router.getAsync('/health-check', async (req: IRequest, res: IResponse) => {
 	res.send('Service is Up!');
 });
 
-export default router.use(BASE_ROUTE, router);
+export default router;
